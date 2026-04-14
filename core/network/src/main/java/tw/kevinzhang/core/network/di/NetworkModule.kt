2282eb61d2affcb970095569f929d7f119b380b6
twkevinzhang <33894721+twkevinzhang@users.noreply.github.com>
2026-04-14 16:24:57 +0800

    feat(core:network): add OkHttpClient Hilt module

---
+++ core/network/src/main/java/tw/kevinzhang/core/network/di/NetworkModule.kt
@@ -0,0 +1,22 @@
+package tw.kevinzhang.core.network.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BASIC
+        })
+        .build()
+}
